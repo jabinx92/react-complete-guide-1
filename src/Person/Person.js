e6cc7d489a386246d99b7277719029b3fc7be030
Jonathan Won <jabinx92@gmail.com>
2020-06-03 18:03:52 -0700

    break

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react"
 
 const person = (props) => {
-    return (
-        <div>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-        </div>
-    )
-};
+return (
+    <div>
+        <p>I'm {props.name} and I  am {props.age} years old!</p>
+        <p>{props.children}</p> {/*children refers to any elements between opening and closing component - ie My Hobbies: Racing*/} 
+    </div>
+)
+}
 
-export default person;
+export default person
