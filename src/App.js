bef9952adad0c17c5285a668a8972d1bc5dfc612
Jonathan Won <jabinx92@gmail.com>
2020-06-09 15:27:51 -0700

    start

--- src/App.js
+++ src/App.js
@@ -144,12 +144,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <ol>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-        </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-
         <form>
           <label>
             Type something:
